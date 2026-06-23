6f8870f14e5ede21d096f3ba20b81158ca5954ac
neha-kamble-05 <150544312+nehaa2121@users.noreply.github.com>
2026-06-23 07:50:28 +0530

    Add files via upload

---
+++ JavaScript/2. Basics/script.js
@@ -0,0 +1,36 @@
+
+//A. Console.log()
+console.log("Hello");
+
+let name = "Neha";
+console.log(name)
+
+//B. alert()
+/*alert("Hi!");
+
+alert("Welcome to JavaScript");*/
+
+//C. document.write
+document.write("Hello, Neha")
+
+
+// Comments in JavaScript
+
+//A. Single-line comment
+
+//Store student name
+let name="Neha";
+
+console.log(name);
+
+//B. Multi-line Comment
+/* This is a
+multi-line comment
+*/
+
+/* 
+Student Information
+Name: Neha
+Age: 20
+*/
+console.log("Student Record")
