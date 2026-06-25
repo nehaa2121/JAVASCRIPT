e537057e328d5cdba22d7ad7facf29a13f1556aa
neha-kamble-05 <150544312+nehaa2121@users.noreply.github.com>
2026-06-25 19:58:33 +0530

    Add files via upload

---
+++ JavaScript/5. conditional statement/elseif.js
@@ -0,0 +1,16 @@
+//eg1 grade calculator
+
+let marks = 85;
+
+if(marks >= 90){
+    console.log("Grade A+");
+}
+else if(marks >= 75){
+    console.log("Grade A");
+}
+else if(marks >=50){
+    console.log("Grade B");
+}
+else{
+    console.log("Fail");
+}
