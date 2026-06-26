90d02e064deb83b3c8d577ab3f336a351ded85e7
neha-kamble-05 <150544312+nehaa2121@users.noreply.github.com>
2026-06-26 19:33:29 +0530

    Add files via upload

---
+++ JavaScript/5. conditional statement/nested.js
@@ -0,0 +1,8 @@
+let age= 20;
+let citizen = true;
+
+if(age>=18){
+    if(citizen){
+        console.log("Can Vote");
+    }
+}
---
+++ JavaScript/5. conditional statement/p1.js
@@ -0,0 +1,8 @@
+//Voting Eligibility
+let age 20;
+if(age >=18){
+    console.log("Eligible");
+}
+else{
+    console.log("Not Eligible");
+}
