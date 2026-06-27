58866bf001d158ac178dbb1117f8d5ca7d1c6d54
neha-kamble-05 <150544312+nehaa2121@users.noreply.github.com>
2026-06-27 18:43:28 +0530

    Add files via upload

---
+++ JavaScript/5. conditional statement/project.js
@@ -0,0 +1,8 @@
+let age= 50;
+
+if(age>=18){
+    document.getElementById("result").innerHTML = "You are eligible to vote...";
+}
+else{
+    document.getElementById("result").innerHTML = "You are not eligible to vote..."
+}
